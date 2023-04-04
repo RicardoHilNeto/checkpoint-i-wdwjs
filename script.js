d8f39572de9bb27e739357c0a5401b4574af4e64
RicardoHilknerNeto <rm550757@fiap.com.br>
2023-04-04 13:44:40 -0300

    Adicionando exercício 1

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Exercicio 1 / Como declarar uma variável com valor undefined em JavaScript? Exemplifique e demonstre a saída do valor da variável!
+// Exercício 1 / Como declarar uma variável com valor undefined em JavaScript? Exemplifique e demonstre a saída do valor da variável!
 let num
 console.log(num)
-
