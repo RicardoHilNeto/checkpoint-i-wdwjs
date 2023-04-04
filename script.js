2091d7dee0af156a6f91e512310de960cb827275
RicardoHilknerNeto <rm550757@fiap.com.br>
2023-04-04 13:48:43 -0300

    Adicionando exercício 2

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // Exercício 1 / Como declarar uma variável com valor undefined em JavaScript? Exemplifique e demonstre a saída do valor da variável!
 let num
 console.log(num)
+
+// Exercicio 2 / Como usar o operador % em JavaScript?Exemplifique e demonstre a saída!
+
+//guardando alguns numeros dentro das variaveis
+let num1 = 10
+let num2 = 5
+
+//guardando o resto da conta em uma variavel
+resultado = !(num1 % num2)
+
+//Condicionais para mostrar se o numero é par ou ímpar
+if(resultado != 0){
+    console.log("O numero é par")
+}else{
+    console.log("O numero é ímpar")
+}
