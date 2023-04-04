8e1a18eff5f9b7edb6c17f6117544f64c77fe2a6
RicardoHilknerNeto <rm550757@fiap.com.br>
2023-04-04 13:51:06 -0300

    Adicionando exercício 3

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ console.log(num)
 //guardando alguns numeros dentro das variaveis
 let num1 = 10
 let num2 = 5
-
+console.log("Fim do exercicio 1")
 //guardando o resto da conta em uma variavel
 resultado = !(num1 % num2)
 
@@ -16,4 +16,24 @@ if(resultado != 0){
     console.log("O numero é par")
 }else{
     console.log("O numero é ímpar")
+}
+console.log("Fim do exercicio 2")
+
+/** Exercicio 3 
+ * Escreva  um  programa  que  calcula  o  índice  de  massa  corporal  (IMC)  de  uma  pessoa  e  exibe  uma
+ * mensagem indicando se ela está abaixo, no peso ideal ou acima do peso. Considere as seguintes faixas de 
+ * IMC:  abaixo  de  18,5  (abaixo  do  peso),  entre18,5  e  24,9  (peso  ideal)  e  acima  de  24,9  (acima  do  peso).
+ * Exemplifique e demonstre a saída!
+*/
+
+//Declarando o valor do índice de massa corporal
+let imc = 20
+
+//Condicionais para mostrar as respostas de acordo com o imc
+if(imc < 18.5){
+    console.log("abaixo  do  peso")
+}else if(imc >= 18.5 && imc <= 24.9){
+    console.log("peso ideal")
+}else{
+    console.log("Acima do peso")
 }
